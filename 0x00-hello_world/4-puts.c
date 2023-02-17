08fa00aca3bca84cdeac717010a9915ff4964d99
Frans-Mathapo <fransman-mathapo@outlook.com>
2023-02-17 21:43:30 +0000

    fix warnings

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * A program that print: 'Programming is like building a multilingual puzzle'
- * Return 0
+ * main - This program prints message using `puts` function.
+ * Return: Always 0 success
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
