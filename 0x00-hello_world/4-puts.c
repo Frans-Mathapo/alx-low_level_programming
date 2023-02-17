056d4b605f94b3317df109b41305888afc7b669f
Frans-Mathapo <fransman-mathapo@outlook.com>
2023-02-17 16:27:49 +0000

    fix the files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(void) {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * A program that print: 'Programming is like building a multilingual puzzle'
+ * Return 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
