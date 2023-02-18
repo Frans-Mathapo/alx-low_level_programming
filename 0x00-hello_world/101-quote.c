e91b5ea43e25ea378497f32519341399ffd6cb90
Frans-Mathapo <fransman-mathapo@outlook.com>
2023-02-18 00:09:35 +0000

    fix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * maim - print a message without printf and puts funtions
+ * Return: 0 success
+ */
+int main(void)
+{
+const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, msg, sizeof(msg) - 1);
+return (1);
+}
