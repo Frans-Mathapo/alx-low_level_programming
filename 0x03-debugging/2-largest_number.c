668eaab1920a2e3601f31da6a169a7d4be720bd8
Frans-Mathapo <frnasman.mathapo@outlook.com>
2023-02-24 00:53:03 +0000

    add

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,24 +10,24 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = c;
-    }
-    else if (b > c)
-    {
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
+	{
 	largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+	}
+	else
+	{
+	largest = c;
+	}
 
-    return (largest);
+	return (largest);
 }
